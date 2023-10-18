04aa1c8b5f2aae459995cdc27bb4d185fc42c694
Jackzz119 <jackzhenghw@gmail.com>
2023-10-17 17:03:34 -0700

    Clear unused code

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,14 +1,6 @@
 import platforms from "../data/platforms";
-import useData from "./useData";
-
-interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
 
 const usePlatforms = () => ({
-  ...useData<Platform>("/platforms"),
   data: platforms,
 });
 
